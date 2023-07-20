13270beeddb1f0247082d1bd6656a7772ac65e8b
이준용 <iiiuooyoo@gmail.com>
2023-07-20 15:01:39 +0900

    feat : 카메라 회전각도 제한

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
-// import { useEffect } from "react";
-// import * as dat from "dat.gui";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Yang } from "./components/yang/Index";
 import { Yin } from "./components/yin/Index";
-// import { Mesh } from "three";
-
-// const Taiji = () => {
-//     const upperHemisphere = React.useRef<Mesh>(null!);
-//     const lowerHemisphere = React.useRef<Mesh>(null!);
-
-//     return (
-//         <group position={[0, 0, 0]}>
-//             <mesh ref={upperHemisphere}>
-//                 <sphereGeometry args={[1, 32, 32, 0, Math.PI]} />
-//                 <meshBasicMaterial attach="material" color="white" side={2} />
-//             </mesh>
-//             <mesh ref={lowerHemisphere}>
-//                 <sphereGeometry args={[1, 32, 32, Math.PI, Math.PI]} />
-//                 <meshBasicMaterial attach="material" color="black" side={2} />
-//             </mesh>
-//         </group>
-//     );
-// };
 
 function App() {
-    // useEffect(() => {
-    //     const gui = new dat.GUI();
-    //     const obj = { x: 0, y: 30 };
-    //     gui.add(obj, "x").min(-10).max(10).step(0.01);
-    //     gui.add(obj, "y").min(0).max(100).step(1);
-    // }, []);
-
     return (
         <Canvas>
-            {/* 3D로 만듦 */}
-            <OrbitControls />
+            <PerspectiveCamera>
+                {/* 3D로 만듦 */}
+                <OrbitControls
+                    enablePan={false}
+                    minPolarAngle={-2}
+                    maxPolarAngle={2}
+                />
+            </PerspectiveCamera>
 
             {/* 광원 */}
             <ambientLight intensity={0.5} />
@@ -46,8 +24,6 @@ function App() {
 
             <Yin />
             <Yang />
-
-            {/* <Taiji /> */}
         </Canvas>
     );
 }
